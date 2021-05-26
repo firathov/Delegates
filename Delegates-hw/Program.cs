418ee48bd377239e516d9431da39bd2332edaff0
firathov <firat.hov@gmail.com>
2021-05-26 21:46:56 +0400

    probably end

--- Delegates-hw/Program.cs
+++ Delegates-hw/Program.cs
@@ -28,94 +28,100 @@ namespace Delegates_hw
     {
         static void Main(string[] args)
         {
-            bool checkerForFirstSwitch;
-            bool checkerForPing;
-            bool checkerForPong;
+            bool checkerForFirstSwitch = true;
+            bool player1 = true;
+            bool player2 = true;
+            bool player3 = true;
+            bool player4 = true;
             do
             {
-                Console.ForegroundColor = ConsoleColor.Magenta;
-                Console.WriteLine("If you want to choose Ping press 1, for choosing Pong press 2");
+                Console.WriteLine("\nChoose the player (1-2-3-4)");
                 byte mainNumber = ForNumberCheck();
                 switch (mainNumber)
                 {
                     case 1:
                         {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            if (player1 == false || player2 == false)
+                            {
+                                Console.WriteLine("This player is in the same team with 2nd one. Pls choose 3rd or 4th one");
+                                checkerForFirstSwitch = true;
+                                break;
+                            }
                             Ping ping = new Ping();
                             ping.Notify += TextPrinter;
                             ping.PingPrint();
-
-                            Console.ForegroundColor = ConsoleColor.Blue;
-                            Console.WriteLine("Do you want to try one more time (for Yes press 1, for No press 2)?");
-                            do
-                            {
-                                byte numberForPing = ForNumberCheck();
-                                switch (numberForPing)
-                                {
-                                    case 1:
-                                        {
-                                            checkerForFirstSwitch = true;
-                                            checkerForPing = false;
-                                            break;
-                                        }
-                                    case 2:
-                                        {
-                                            Console.WriteLine("See ya");
-                                            checkerForFirstSwitch = false;
-                                            checkerForPing = false;
-                                            break;
-                                        }
-                                    default:
-                                        {
-                                            Console.WriteLine("Wrong symbol, pls try again");
-                                            checkerForFirstSwitch = true;
-                                            checkerForPing = true;
-                                            break;
-                                        }
-                                }
-
-                            } while (checkerForPing == true) ;
+                            player1 = false;
+                            player3 = true;
+                            player4 = true;
+                            
                             break;
                         }
                     case 2:
                         {
-                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            if (player1 == false || player2 == false)
+                            {
+                                Console.WriteLine("This player is in the same team with 1st one. Pls choose 3rd or 4th one");
+                                checkerForFirstSwitch = true;
+                                break;
+                            }
+                            Ping ping2 = new Ping();
+                            ping2.Notify += TextPrinter;
+                            ping2.PingPrint();
+                            player2 = false;
+                            player3 = true;
+                            player4 = true;
+                            
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.ForegroundColor = ConsoleColor.Magenta;
+                            if (player3 == false || player4 == false)
+                            {
+                                Console.WriteLine("This player is in the same team with 4th one. Pls choose 1st or 2nd one");
+                                checkerForFirstSwitch = true;
+                                break;
+                            }
                             Pong pong = new Pong();
                             pong.Notify += TextPrinter;
                             pong.PongPrint();
-
-                                Console.ForegroundColor = ConsoleColor.Green;
-                                Console.WriteLine("Do you want to try one more time (for Yes press 1, for No press 2)?");
-                            do
+                            player3 = false;
+                            player1 = true;
+                            player2 = true;
+                            
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            if (player4 == false || player3 == false)
                             {
-                                byte numberForPong = ForNumberCheck();
-                                switch (numberForPong)
-                                {
-                                    case 1:
-                                        {
-                                            checkerForFirstSwitch = true;
-                                            checkerForPong = false;
-                                            break;
-                                        }
-                                    case 2:
-                                        {
-                                            Console.WriteLine("See ya");
-                                            checkerForFirstSwitch = false;
-                                            checkerForPong = false;
-                                            break;
-                                        }
-                                    default:
-                                        {
-                                            Console.WriteLine("Wrong symbol, pls try again");
-                                            checkerForFirstSwitch = true;
-                                            checkerForPong = true;
-                                            break;
-                                        }
-                                }
-                            } while (checkerForPong == true);
+                                Console.WriteLine("This player is in the same team with 3rd one. Pls choose 1st or 2nd one");
+                                checkerForFirstSwitch = true;
+                                break;
+                            }
+                            
+                            Pong pong2 = new Pong();
+                            pong2.Notify += TextPrinter;
+                            pong2.PongPrint();
+                            player4 = false;
+                            player1 = true;
+                            player2 = true;
+
+                            break;
+                        }
+                    case 5:
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("See yaa");
+                            checkerForFirstSwitch = false;
                             break;
                         }
                     default:
                         {
+                            Console.ForegroundColor = ConsoleColor.Red;
                             Console.WriteLine("Wrong symbol, pls try again");
 
                             checkerForFirstSwitch = true;
